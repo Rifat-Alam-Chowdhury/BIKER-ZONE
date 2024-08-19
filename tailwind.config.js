bbbf09ce591ce2cab9aef9a03d2540c348a26839
rifat chowdhury <rifatalamtabs2@gmail.com>
2024-08-20 01:26:57 +0600

    biker xone

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            'bike-primary':'rgba(231, 111, 81, 0.1)',
+        },
+      },
+    },
+    plugins: [],
+  }
